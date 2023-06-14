1a04ce4089b0f26aed2700f95c0fe1f9a315a8c4
LyCrash <kl_mouhoun@esi.dz>
2023-06-14 17:36:33 +0100

    Fixing the box size

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -5,7 +5,7 @@ import PostBox from "@/components/shared/PostBox";
 const Events: NextPage = () => {
     return (
         <OperationsLayout pageName="events">
-            <div className="grid grid-cols-4 gap-x-10 gap-y-16 my-11 mx-16">
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-10 gap-y-16 my-11 mx-24 ">
                 <PostBox></PostBox>
                 <PostBox></PostBox>
                 <PostBox></PostBox>
