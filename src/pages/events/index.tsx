06e9b684e4370c599ce36b76e7cceb548ee4a778
LyCrash <kl_mouhoun@esi.dz>
2023-06-17 18:18:31 +0100

    Integrating the event page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,26 +1,34 @@
 import OperationsLayout from "@/components/shared/OperationsLayout";
 import { NextPage } from "next";
 import PostBox from "@/components/shared/PostBox";
+import axios from "@/config/axios";
+import { useEffect, useState } from "react";
+import getListEvents from "@/services/events.service.js"
+
 
 const Events: NextPage = () => {
+   
+    const [listEvent, setListEvent]= useState<any>(null); 
+    
+    useEffect(() => {
+        const fetchListevent = async () => {
+            const events = await getListEvents();
+            setListEvent(events);
+        } 
+        fetchListevent();
+    }, [])
     return (
         <OperationsLayout pageName="events">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-10 gap-y-16 my-11 mx-24 ">
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
-                <PostBox></PostBox>
+                
+                {listEvent.map((event: any)=>{
+            
+                    return <PostBox image={event.chemin} title={event.titre} desc={event.description}></PostBox>
+                })}
+
             </div>
         </OperationsLayout>
     );
 }
- 
+
 export default Events;
