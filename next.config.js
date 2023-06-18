3e72a61ef1a2906530ea8057777a4cca31e38a04
AMROUCHE Maroua <116122687+AmroucheMaroua@users.noreply.github.com>
2023-06-18 01:31:00 +0100

    pictures config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.googleusercontent.com',
-			},
-		],
-	},
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.tripadvisor.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
-
